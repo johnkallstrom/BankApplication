b1f3772c9ca1dc52ebd4610f1271d1dda193e2f4
johnkallstrom <jekallstrom@gmail.com>
2020-04-25 14:01:10 +0200

    added controllers and views folders.

---
+++ Bank.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
