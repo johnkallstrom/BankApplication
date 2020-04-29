7fa00d185d62dc0004eed0fd3e47f9a245f377aa
johnkallstrom <jekallstrom@gmail.com>
2020-04-29 12:22:38 +0200

    fixes.

--- Bank.Web/Controllers/HomeController.cs
+++ Bank.Web/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bank.Web.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bank.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IBankStatisticsService _bankStatisticsService;
+
+        public HomeController(IBankStatisticsService bankStatisticsService)
+        {
+            _bankStatisticsService = bankStatisticsService;
+        }
+
         public IActionResult Index()
         {
             return View();
