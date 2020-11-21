5358012a6937e0626a88060885b873462be7a962
John Källström <jekallstrom@gmail.com>
2020-11-21 21:02:56 +0100

    responsive home page

--- Bank.Web/wwwroot/js/script.js
+++ Bank.Web/wwwroot/js/script.js
@@ -8,3 +8,12 @@
         return uri + separator + key + "=" + value;
     }
 };
+
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
